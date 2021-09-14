907096f60448ea198b4f29d5b86d88885f490e20
Rakaesa <54375203+Rakaesa@users.noreply.github.com>
2021-09-14 17:25:53 -0400

    dao basics
    Set up all the fundamentals for the DAO layer.

---
+++ src/main/java/dao/VendingMachineDaoImpl.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author conno
+ */
+public class VendingMachineDaoImpl implements VendingMachineDao {
+
+    public static final String DELIMITER = "::";
+    private final String MACHINE_FILE;
+
+    private Map<String, Item> items = new HashMap<>();
+
+    public VendingMachineDaoImpl() {
+        MACHINE_FILE = "vendingMachine.txt";
+    }
+
+    private String marshallStudent(Item i) {
+        return i.getName() + DELIMITER + i.getPrice() + DELIMITER + i.getStock();
+    }
+    
+    public Item unMarshallItem(String itemInf){
+
+        String[] itemVariables = itemInf.split(DELIMITER);
+        Item newItem = new Item(itemVariables[0], itemVariables[1], itemVariables[2]);
+        return newItem;
+    }
+
+    private void writeMachine() throws VendingMachinePersistenceException {
+
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(MACHINE_FILE));
+        } catch (IOException e) {
+            throw new VendingMachinePersistenceException(
+                    "Failed to write item data.", e);
+        }
+
+        String itemAsText;
+        List<Item> itemList = this.getAllItems();
+        for (Item i : itemList) {
+
+            itemAsText = marshallItem(i);
+            out.println(itemAsText);
+            out.flush();
+        }
+        out.close();
+    }
+
+    private void loadMachine() throws VendingMachinePersistenceException {
+        Scanner scanner;
+        try {
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(MACHINE_FILE)));
+        } catch (FileNotFoundException e) {
+            throw new VendingMachinePersistenceException(
+                    "Could not load items into memory.", e);
+        }
+        
+        String currentLine;
+        Item currentItem;
+        while (scanner.hasNextLine()) {
+            currentLine = scanner.nextLine();
+            currentItem = unMarshallItem(currentLine);
+
+            // We are going to use the student id as the map key for our student object.
+            // Put currentStudent into the map using student id as the key
+            items.put(currentItem.getName(), currentItem);
+        }
+        // close scanner
+        scanner.close();
+    }
+
+    @Override
+    public List<Item> getAllItems() throws VendingMachinePersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Item getItem(String name) throws VendingMachinePersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void decreaseStock(Item item) throws VendingMachinePersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
